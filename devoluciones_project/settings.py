3bb0f679e761b5ade0e137ac220fd197955d9c4b
Sanjay Ramchandani <sanjay@hotmail.es>
2025-04-21 10:15:41 -0400

    update settings

--- devoluciones_project/settings.py
+++ devoluciones_project/settings.py
@@ -71,8 +71,7 @@ WSGI_APPLICATION = 'devoluciones_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=True
+        default=config('DATABASE_URL')
     )
 }
 
