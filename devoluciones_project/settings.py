9d8ece0492565c4372a450455a2c3e06b9dbffaf
Sanjay Ramchandani <sanjay@hotmail.es>
2025-04-21 10:13:02 -0400

    update settings

--- devoluciones_project/settings.py
+++ devoluciones_project/settings.py
@@ -69,12 +69,13 @@ WSGI_APPLICATION = 'devoluciones_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 
 
 # Password validation
@@ -176,5 +177,3 @@ LOGGING = {
         'level': 'INFO',
     },
 }
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
